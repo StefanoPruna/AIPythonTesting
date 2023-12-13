2a37fa26585214d9a732061c4bd3db08264e282c
StefanoPruna <stefano.pruna@googlemail.com>
2023-12-13 13:43:54 +1100

    first commit

---
+++ Start.py
@@ -0,0 +1,57 @@
+import numbers
+
+#1. Creating the function and reading the message file
+# def decode():
+#     f = open("message_file.txt", "r")
+#     print(f.read())
+ 
+# decode()
+
+#2. Loop each row to separate the numbers to the words, then print the numbers only
+# def decode(n):
+#     f = open("message_file.txt", "r")
+#     onlyNumbers = []
+#     for line in f:
+#         for subitem in line.split():
+#             if(subitem.isdigit()):
+#                 onlyNumbers.append(subitem)
+       
+#     onlyNumbers.sort()
+    
+#     #pyramid code
+#     num=1
+#     list =[]
+#     for i in range(0,n):
+#         for j in range(0, i+1):            
+#             print(num, end=" ")
+#             num+=1
+
+#         print("\r")   
+    
+#     return list
+#     #return onlyNumbers
+# n=3
+# print('\n'.join(decode(n)))
+
+#3. Create the dictionary
+def decode():
+    f = open("message_file.txt", "r")
+    onlyNumbers = {}
+    for line in f:
+        (key, val) = line.split()
+        onlyNumbers[int(key)] = val
+            
+    sortedDic = dict(sorted(onlyNumbers.items()))
+    sortedDic.pop(2)
+    sortedDic.pop(4)
+    sortedDic.pop(5)
+    
+    for k, v in sortedDic.items():
+        print(k,':',v)
+        
+    for values in sortedDic.values():
+        print(values)
+                 
+    return sortedDic.values()
+
+print(decode())
